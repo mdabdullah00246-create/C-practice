13c9648e5ab012652d8a131bf57b8b691ce54efa
Md Abdullah <mdabdullah00246@gmail.com>
2026-07-18 20:07:59 +0530

    Added um of arrrays

--- chap7.c
+++ chap7.c
@@ -4,12 +4,15 @@ int main(){
     int a[5];
     printf("Enter 5 number:\n");
     int i;
+    int sum=0;
     for(i=0; i<5; i++){
         scanf("%d",&a[i]);
+        sum=sum+a[i];
     }
-    printf("Elements are:\n");
-    for(i=0; i<5; i++){
-        printf("%d\n",a[i]);
-    }
+    printf("The sum of numbers is %d",sum);
+    // printf("Elements are:\n");
+    // for(i=0; i<5; i++){
+    //     printf("%d\n",a[i]);
+    // }
     return 0;
 }
