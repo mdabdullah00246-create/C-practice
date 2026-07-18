60e8b47df1f4c641f6c5c81028bc92b63c4bc90c
Md Abdullah <mdabdullah00246@gmail.com>
2026-07-18 20:25:13 +0530

    Modified and Added Largest of numbers

--- chap7.c
+++ chap7.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
 int main(){
-    int a[5];
-    printf("Enter 5 number:\n");
-    int i;
-    int sum=0;
-    for(i=0; i<5; i++){
-        scanf("%d",&a[i]);
-        sum=sum+a[i];
-    }
-    printf("The sum of numbers is %d",sum);
+    // int a[5];
+    // printf("Enter 5 number:\n");
+    // int i;
+    // int sum=0;
+    // for(i=0; i<5; i++){
+    //     scanf("%d",&a[i]);
+    //     sum=sum+a[i];
+    // }
+    // printf("The sum of numbers is %d",sum);
     // printf("Elements are:\n");
     // for(i=0; i<5; i++){
     //     printf("%d\n",a[i]);
     // }
+    int a[5];
+    printf("Enter 5 number:");
+    int i;
+    for(i=0; i<5; i++){
+        scanf("%d",&a[i]);
+    }
+        if(a[0]>a[1] && a[0]>a[2] && a[0]>a[3] && a[0]>a[4]){
+            printf("%d is Largest",a[0]);
+        }
+        else if(a[1]>a[0] && a[1]>a[2] && a[1]>a[3] && a[1]>a[4]){
+            printf("%d is Largest",a[1]);
+        }
+        else if(a[2]>a[0] && a[2]>a[1] && a[2]>a[3] && a[2]>a[4]){
+            printf("%d is Largest",a[2]);
+        }
+        else if(a[3]>a[0] && a[3]>a[2] && a[3]>a[1] && a[3]>a[4]){
+            printf("%d is Largest",a[3]);
+        }
+        else{
+            printf("%d is Largest",a[4]);
+        }
+    
     return 0;
 }
