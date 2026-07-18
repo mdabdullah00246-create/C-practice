111f84bbc8a7735cc93dc0c7d0f117c7b5ec94e9
Md Abdullah <mdabdullah00246@gmail.com>
2026-07-18 19:06:04 +0530

    Added swap Number program

--- chap6.c
+++ chap6.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+// int sum(int ,int);
+// int sum(int a, int b){
+//     return a+b;
+// }
+// int sum(int*,int*);
+// int sum(int*a,int*b){
+//     *a=8;
+//     return *a+*b;
+// }
+void swap(int * , int*);
+void swap(int*a , int*b){
+    int temp= *a;
+    *a=*b;
+    *b=temp;   
+}
 int main(){
-    int i;
-    int* j= &i;
-    printf("The address of i is %p\n",j);
-    printf("The address of i is %p",j);
-    printf("Hde/;gjse.nvesl");
+   
+    int x=80;
+    int y=90;
+    printf("The Value of x and y Before swap is\nx=%d\ny=%d\n",x,y);
+    swap(&x,&y);
+    printf("The value of x and y after swap is\nx=%d\ny=%d\n",x,y);
     return 0;
 }
