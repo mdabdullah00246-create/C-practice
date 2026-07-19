ff07c45187f73ce41d4a3b0529910db50f2bb026
Md Abdullah <mdabdullah00246@gmail.com>
2026-07-19 19:20:29 +0530

    Added smallest of number program

--- chap7.c
+++ chap7.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     // int a[5];
     // printf("Enter 5 number:\n");
     // int i;
@@ -14,37 +15,52 @@ int main(){
     // for(i=0; i<5; i++){
     //     printf("%d\n",a[i]);
     // }
+    // int n;
+    // printf("Enter Number of numbers=");
+    // scanf("%d",&n);
+    // int a[n];
+    // printf("Enter all numbers:");
+    // int i;
+    // for(i=0; i<n; i++){
+    //     scanf("%d",&a[i]);
+    // }
+    // if(a[0]>a[1] && a[0]>a[2] && a[0]>a[3] && a[0]>a[4]){
+    //     printf("%d is Largest",a[0]);
+    // }
+    // else if(a[1]>a[0] && a[1]>a[2] && a[1]>a[3] && a[1]>a[4]){
+    //     printf("%d is Largest",a[1]);
+    // }
+    // else if(a[2]>a[0] && a[2]>a[1] && a[2]>a[3] && a[2]>a[4]){
+    //     printf("%d is Largest",a[2]);
+    // }
+    // else if(a[3]>a[0] && a[3]>a[2] && a[3]>a[1] && a[3]>a[4]){
+    //     printf("%d is Largest",a[3]);
+    // }
+    // else{
+    //     printf("%d is Largest",a[4]);
+    // }
+    // int largest=a[0];
+    // for(i=1; i<n; i++){
+    //     if(a[i]>largest){
+    //         largest=a[i];
+    //     }
+    // }
+    // printf("Largest Number is %d", largest);
     int n;
-    printf("Enter Number of numbers=");
+    printf("Enter a Number of Numbers=");
     scanf("%d",&n);
     int a[n];
-    printf("Enter all numbers:");
     int i;
     for(i=0; i<n; i++){
         scanf("%d",&a[i]);
     }
-        // if(a[0]>a[1] && a[0]>a[2] && a[0]>a[3] && a[0]>a[4]){
-        //     printf("%d is Largest",a[0]);
-        // }
-        // else if(a[1]>a[0] && a[1]>a[2] && a[1]>a[3] && a[1]>a[4]){
-        //     printf("%d is Largest",a[1]);
-        // }
-        // else if(a[2]>a[0] && a[2]>a[1] && a[2]>a[3] && a[2]>a[4]){
-        //     printf("%d is Largest",a[2]);
-        // }
-        // else if(a[3]>a[0] && a[3]>a[2] && a[3]>a[1] && a[3]>a[4]){
-        //     printf("%d is Largest",a[3]);
-        // }
-        // else{
-        //     printf("%d is Largest",a[4]);
-        // }
-        int largest=a[0];
-        for(i=1; i<n; i++){
-            if(a[i]>largest){
-                largest=a[i];
-            }
+     int smallest=a[0];
+    for(i=0; i<n; i++){
+        if(a[i]<smallest){
+            smallest=a[i];
         }
-        printf("Largest Number is %d", largest);
-    
-    return 0;
+    }
+    printf("%d is smallest",smallest);
+
+        return 0;
 }
